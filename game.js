1c0de3f651efb351ff082acf209aade05fcc931a
abhaydevelops <abhaysinghlko29@gmail.com>
2024-04-02 17:59:42 +0530

    updated js

--- game.js
+++ game.js
@@ -55,14 +55,14 @@ function animatePress(currentColour) {
 
 function checkAnswer(currentLevel) {
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-    // console.log("success");
+    
     if (userClickedPattern.length === gamePattern.length) {
       setTimeout(function () {
         nextSequence();
       }, 1000);
     }
   } else {
-    // console.log("wrong");
+   
     
     playSound("wrong");
 
